9c029e5248780fa59c8773822b82577cd70922de
btcguide <115478243+btcperfectguide@users.noreply.github.com>
2026-03-05 17:52:23 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:51:55.017Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T14:52:22.653Z
+Commit: Fix CSS styling bug on mobile devices
