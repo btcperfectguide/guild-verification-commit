d9f244cd0d0b917b3d6e5d7bc210543e4970ff15
btcguide <115478243+btcperfectguide@users.noreply.github.com>
2026-03-05 23:29:08 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:52:22.653Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:29:07.635Z
+Commit: Fix broken links in documentation
